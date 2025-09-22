0402b26aa0b638767cce2a7caed10fd3aee658e0
elliebennings <benninej@mail.uc.edu>
2025-09-22 19:05:24 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double INTREST_RATE = 0.17;
+        int balance = 5000;
+
+        int balance = balance + balance * INTREST_RATE;
+
+        System.out.println("Your balance after one month is: " + balance);
+        System.out.println("Your balance after two months is: " + balance);
+
+
+    }
+}
